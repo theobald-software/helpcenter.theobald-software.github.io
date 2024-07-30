b79bd40952e3b733099c95c987310b9c849cb183
Valerie Schipka <schipka@theobald.local>
2024-07-30 08:11:37 +0200

    broken images + missing title & styling

--- projects/board-connector/docs/assets/javascripts/changelog.js
+++ projects/board-connector/docs/assets/javascripts/changelog.js
@@ -74,10 +74,10 @@ document.addEventListener('DOMContentLoaded', () => {
 			row.innerHTML = `
 			<td>${firstChange ? item.Version : ''}</td>
 			<td>${firstChange ? item.LegacyReleaseDate.split(' ')[0] : ''}</td>
-			<td>${change.Component}</td>
-			<td>
-			  ${firstChange && item.IsBreaking ? '<img src="../version-history-new/link_broken.svg" alt="breaking-change" style="width:20px;">' : ''}
-			  ${firstChange && item.IsCritical ? '<img src="../version-history-new/critical.svg" alt="breaking-change" style="width:20px;">' : ''}
+			<td>${change.Component} <br>(${change.Product})</td>
+			<td style="width:5%;text-align:center;">
+			  ${firstChange && item.IsBreaking ? '<img src="../assets/images/logos/link_broken.svg" alt="breaking-change" title="Breaking Change: This update affects (breaks) your existing extraction setup.  Be sure to test this update on a QA environment, before updating your production environment. Read the Release Note to understand if and how your extractions are affected by this update." style="width:20px;">' : ''}
+			  ${firstChange && item.IsCritical ? '<img src="../assets/images/logos/critical.svg" alt="critical-change" title="Critical Change: This is an important software release. Installing this update is highly recommended." style="width:20px;">' : ''}
 			</td>
 			<td>${change.Message} ${change.ReleaseNote ? ` <button class="show-more" data-release-note="${encodeURIComponent(change.ReleaseNote)}" data-product-name="${change.Product}" data-version="${item.Version}" style="cursor: pointer; color: #ED1A33;">(Open Release note)</button>` : ''}</td>
 		  `;
